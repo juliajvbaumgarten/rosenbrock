700f6852a3f05d858de8da32cd3cf7ae56c87837
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-10-23 12:43:32 -0400

    Implement Nelder-Mead optimization function
    Added a new Nelder-Mead optimization function with author information.

--- nelder-mead.py
+++ nelder-mead.py
@@ -1,1 +1,6 @@
+Author: Lauren Sdun, Julia Jones, Julia Baumgarten
 
+def nelder_mead(x0: np.ndarray, max_iter: int = 2000, tol: float = 1e-10) -> Result:
+    res = minimize(rosen, x0, method="Nelder-Mead",
+                   options={"maxiter": max_iter, "xatol": tol, "fatol": tol, "disp": False})
+    return Result("nelder_mead", x0, res.x, res.fun, res.nit, res.success, {"message": res.message})
