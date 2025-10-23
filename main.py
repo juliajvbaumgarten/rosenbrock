84eeccfb484e470f1581ad6d5ada6ef2cf523ed0
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-10-23 12:51:30 -0400

    Add imports and configuration for optimization

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import numpy as np
+from dataclasses import dataclass
+from typing import Callable, Tuple, Dict, Any
+from numpy.linalg import norm
+from scipy.optimize import minimize
+import rosenbrockfct as rosen
+
+
+# Configuration
+
+X_RANGE = (-2.0, 2.0)
+Y_RANGE = (-1.0, 3.0)
+
+@dataclass
+class Result:
+    method: str
+    x0: np.ndarray
+    x: np.ndarray
+    f: float
+    iters: int
+    success: bool
+    info: Dict[str, Any]
 
