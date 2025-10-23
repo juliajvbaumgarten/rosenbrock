732994f37f0f39d7c19ca4dada3d9c7bb062cd53
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-10-23 12:59:39 -0400

    Update nelder_mead.py

--- nelder_mead.py
+++ nelder_mead.py
@@ -1,5 +1,3 @@
-# Author: Lauren Sdun, Julia Jones, Julia Baumgarten
-
 def nelder_mead(x0: np.ndarray, max_iter: int = 2000, tol: float = 1e-10) -> Result:
     res = minimize(rosen, x0, method="Nelder-Mead",
                    options={"maxiter": max_iter, "xatol": tol, "fatol": tol, "disp": False})
